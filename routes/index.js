2fb7d1699e6f8927ba429859c51c64825cbb5ffe
Elisangela Aps <elisangela.aps_ti@hotmail.com>
2023-01-26 19:45:06 -0300

    primeira versão do ultimo  projeto dev web

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
-const express = require('express'); //exportação express
+const express = require('express'); 
 
 const router = express.Router();
+
 router.get('/', (req, res) => {
     let obj = {
         'nome': req.query.nome
     };
     res.render('home', obj);
+
+});
+
+router.get('/pagina2', (req, res) => {
+    let obj = {
+        'nome': req.query.nome
+    };
+    res.render('page_secundaria', obj);
+
+});
+
+router.get('/pagina3', (req, res) => {
+    let obj = {
+        'nome': req.query.nome
+    };
+    res.render('page_terciaria', obj);
 
 });
 
