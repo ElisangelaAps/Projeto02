117e1c1c66886b15c67fa6aa7908386f152a382d
Elisangela Aps <elisangela.aps_ti@hotmail.com>
2023-01-27 19:15:22 -0300

    finalização do projeto

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,15 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     let obj = {
-        'nome': req.query.nome
-    };
+        disciplinas: [
+            {nome: 'Gestão Ágil de Projetos', dia: 'Segunda-Feira', horario: '07:40h às 11:20h'},
+            {nome: 'Banco de Dados não relacional', dia: 'Terça-Feira',horario: '07:40h às 09:20h'},
+            {nome: 'Inglês 1', dia: 'Terça-Feira', horario: '11:20h às 13:00h'},
+            {nome: 'Desenvolvimento Web III', dia: 'Quarta-feira', horario: '07:40h às 09:20h'},
+            {nome: 'Interação Humano Computador',dia: 'Quarta-feira',horario:'11:20h às 13:00h'},
+            {nome: 'Álgebra Linear',dia: 'Quinta-feira',horario: '07:40h ás 11:20h'},
+            {nome: 'Técnicas de Programação II',dia:'Sexta-Feira',horario: '07:40h às 11:20h'}
+    ]};
     res.render('home', obj);
 
 });
